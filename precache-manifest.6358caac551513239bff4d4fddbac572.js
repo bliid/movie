6249633cad43a37debaeba9845a4b352742180a9
bliid <gh.bliid@gmail.com>
2019-09-13 23:33:42 +0100

    Updates

---
+++ precache-manifest.6358caac551513239bff4d4fddbac572.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0164c8a54785310d9e8af5ba45bf1891",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "4360d9448e1906dde7c7",
+    "url": "/movie/static/css/main.82fa2916.chunk.css"
+  },
+  {
+    "revision": "9cad6b522d345e4f33f5",
+    "url": "/movie/static/js/2.b51f4e42.chunk.js"
+  },
+  {
+    "revision": "4360d9448e1906dde7c7",
+    "url": "/movie/static/js/main.a714bff1.chunk.js"
+  },
+  {
+    "revision": "126c55c9fefdbdc1ae20",
+    "url": "/movie/static/js/runtime~main.2c2cae20.js"
+  }
+]);
